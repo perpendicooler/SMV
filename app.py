1d9be74644672026d5a26cc786f8e8fcdab20c23
MD ARIF HOSSAIN <arif.hossain.shakib@gmail.com>
2024-10-17 12:39:14 +0600

    Update app.py

--- app.py
+++ app.py
@@ -121,10 +121,10 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs(["🏠SMV Prediction App", "🚀Overview"
                                         "📊Data Preparation", "💻Modeling", "📈Results"])
 
 with tab1:
-    st.markdown(
-        f"<div style='text-align: center;'><img src='IND Logo PNG +.png' width='500'></div>",
-        unsafe_allow_html=True
-    )
+    col1, col2, col3 = st.columns([1, 2, 1])  # Adjust the proportions as needed
+
+    with col2:  # Center column
+        st.image("IND Logo PNG +.png", width=500)
 
     st.markdown('<h1 class="title">SMV Prediction App</h1>', unsafe_allow_html=True)
 
