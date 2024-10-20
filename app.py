d1086d943e6c0306e0f0964612f58147fb4a70e9
MD ARIF HOSSAIN <arif.hossain.shakib@gmail.com>
2024-10-20 12:50:21 +0600

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 import joblib
 
 # Add custom CSS for styling with enhancements and animations
+# Inject custom styles
 st.markdown(
     """
     <style>
@@ -277,7 +278,7 @@ with tab1:
                       #  st.write(f"**On average, the SMV is estimated to be around** {combined_prediction:.2f}")
             
                     # Save prediction to Excel
-                    if st.button("Save Prediction"):
+                    if middle.button("Save Prediction"):
                         predictions_df = pd.DataFrame({
                             'GG': [GG],
                             'Operation': [Operation],
